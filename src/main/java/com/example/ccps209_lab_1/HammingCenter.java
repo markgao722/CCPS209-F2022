f2af4f20fd386e844a007f5d7298fd6e113864aa
Mark <45904093+markgao722@users.noreply.github.com>
2022-10-01 22:57:05 -0400

    Laptop commit

--- src/main/java/com/example/ccps209_lab_1/HammingCenter.java
+++ src/main/java/com/example/ccps209_lab_1/HammingCenter.java
@@ -1,4 +1,5 @@
 package com.example.ccps209_lab_1;
+import java.util.Arrays;
 
 public class HammingCenter {
     /* Summary: the Hamming Distance between two arrays is the number of positions in which they differ.
@@ -15,11 +16,31 @@ public class HammingCenter {
      * The solution should be the lexicographically lowest solution, e.g. where false precedes true.
      * @param bits An array of boolean arrays, which have the same length
      * @param r The maximum radius of the Hamming Center
-     * @return Return the Hamming Center array, or null if no solution exists for bits and r
+     * @return Return the Hamming Center array, or null if no solution exists for these bits and r
      */
     public static boolean[] findHammingCenter(boolean[][] bits, int r) {
         boolean[] hamCenter;
+        int[] distances;
+        int k = 0;
 
+        // Unnecessarily verbose to help with understanding
+        int aSize = bits[0].length;
+        hamCenter = new boolean[aSize];
+        distances = new int[aSize];
+
+        while (k < aSize) {
+            hamCenter[k]  = findCenter(bits, distances, hamCenter, k, r);
+
+            int maxDist = Arrays.sort(distances)[0]; //why can't this see distances from the outer scope??
+
+            if (maxDist > r) {
+                return null;
+                    // instructions say to put this in helper method...how will we know if false = no solution?
+            } else {
+                k++;
+            }
+
+        }
         return hamCenter;
     }
 
@@ -30,10 +51,28 @@ public class HammingCenter {
      * @param center The current iteration's (i) Hamming Center solution
      * @param k The current element in the current Hamming Center solution, from 0 to center.length
      * @param r The maximum radius of the Hamming Center
-     * @return
+     * @return Return the value (T/F) that should be used in this k-th spot in the Hamming array
      */
     private static boolean findCenter(boolean[][] bits, int[] distance, boolean[] center, int k, int r) {
+        int falseGoodness;
+        int trueGoodness;
+        int[] falseDistances = Arrays.copyOf(distance, distance.length);
+        int[] trueDistances = Arrays.copyOf(distance, distance.length);
 
-        return false;
+        for (int i = 0; i < bits.length; i++) {
+            if (bits[i][k]) {
+                falseDistances[i]++;  // assume center[k] is False; if bits[i][k] is True, +1 distance
+            } else {
+                trueDistances[i]++;
+            }
+        }
+
+        falseGoodness = Arrays.stream(falseDistances).sum();
+        trueGoodness = Arrays.stream(trueDistances).sum();
+        if (falseGoodness < trueGoodness) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
