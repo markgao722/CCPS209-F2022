061e34eb17948eacfdb05162a4254df3d93723ca
Mark <markgao5059@outlook.com>
2022-10-14 19:32:57 -0400

    Set up a couple more labs

---
+++ src/main/java/com/example/ccps209_lab_1/P2J4.java
@@ -0,0 +1,20 @@
+package com.example.ccps209_lab_1;
+
+public class P2J4 {
+
+    public static List<Integer> runningMedianOfThree(List<Integer> items) {
+
+    }
+
+    public static int firstMissingPositive(List<Integer> items) {
+
+    }
+
+    public static void sortByElementFrequency(List<Integer> items) {
+
+    }
+
+    public static List<Integer> factorFactorial(int n) {
+
+    }
+}
---
+++ src/main/java/com/example/ccps209_lab_1/P2J7.java
@@ -0,0 +1,12 @@
+package com.example.ccps209_lab_1;
+
+public class P2J7 {
+
+    public static <T> List<T> josephus(List<T> men, int k) {
+
+    }
+
+    public static int[] huntingtonHill(int[] population, int seats) {
+
+    }
+}
