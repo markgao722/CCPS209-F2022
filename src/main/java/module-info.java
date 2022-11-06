144c013b0922e4c48e4532e370ff2323daa590a8
Mark <45904093+markgao722@users.noreply.github.com>
2022-11-06 13:14:51 -0500

    Maven setup on laptop, test files working. Module info file changed.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.ccps209_lab_1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires junit;
+    requires junit.platform.console.standalone;
 
 
     opens com.example.ccps209_lab_1 to javafx.fxml;
