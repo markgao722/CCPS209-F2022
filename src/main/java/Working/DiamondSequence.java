88430af4788f350604f8ca33e7513dca67abe56d
Mark <45904093+markgao722@users.noreply.github.com>
2022-11-12 22:15:09 -0500

    Diamond sequence first draft

--- src/main/java/Working/DiamondSequence.java
+++ src/main/java/Working/DiamondSequence.java
@@ -1,7 +1,7 @@
 package Working;
 
 import java.util.Iterator;
-import java.lang.Math;
+import static java.lang.Math.toIntExact;
 
 public class DiamondSequence implements Iterator<Integer> {
     /* Summary: The NatSet library is a kind of Set<E> used to track the natural numbers.
@@ -9,27 +9,34 @@ public class DiamondSequence implements Iterator<Integer> {
         The next element a(k) = the smallest new positive integer such that the first k elements of the
             sequence sum to an integer multiple of k.
         Example: [1,3,2,6,8,4,11,5,14,16,7,19,21,9,...]
-        hrs: 0
+        hrs: 2
      */
 
     public boolean hasNext() { return true; }
 
-    private Long k = 0L;  // current position of the sequence generated thus far
+    private Long k = 1L; //the sequence generated thus far
     private Long sum = 0L;  // sum of the sequence generated thus far
-    private NatSet seen = new NatSet();  // integers seen so far in the sequence generated thus far
+    private final NatSet seen = new NatSet();  // integers seen so far in the sequence generated thus far
+    // [0,1,2,3,4,5,6,7,...]
+    // [T,T,T,F,F,F,F,F,...]
 
     /**
      * Returns the next integer in the Diamond Sequence (see definition). Each instance is a new sequence.
      * @return Returns the next integer in the sequence
      */
     public Integer next() {
-        Integer nextVal = 1;
+        //Q1 is conversion to Integer last minute ok?
+        //Q2 do we represent number 1 in the zero idx position?
+        long testVal;
         this.k++;
 
-        for(int i = Math.toIntExact(this.k); i < this.k; i++) {
-            
+        // check for smallest unseen integer I, if it satisfies sum+I % k == 0 use it, if not, keep looking
+        testVal = seen.allTrueUpTo() + 1L;
+        while(this.sum + testVal % this.k != 0) {
+            testVal += 1;
         }
-
-        return nextVal;
+        sum += testVal;
+        seen.add(testVal);
+        return toIntExact(testVal);
     }
 }
