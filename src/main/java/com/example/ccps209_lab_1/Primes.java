56b87027137f97edc3151ac2cfafd07b92115e1a
Mark <markgao5059@outlook.com>
2022-12-03 13:19:40 -0500

    sieve method??

--- src/main/java/com/example/ccps209_lab_1/Primes.java
+++ src/main/java/com/example/ccps209_lab_1/Primes.java
@@ -1,11 +1,12 @@
 package com.example.ccps209_lab_1;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 public class Primes {
     /* Summary: A variety of basic operations to work with prime numbers.
-        hrs: 0
+        hrs: 1
      */
 
     /**
@@ -14,9 +15,18 @@ public class Primes {
      * @return Returns True if n is prime
      */
     public static boolean isPrime(int n) {
-        // Trial division: if n has a non-prime factor, one of them must be <= sqrt(n), so
+        // Trial division: if n has a nontrivial factor, one of them must be <= sqrt(n), so
         //  there's no need to check for factors greater than sqrt(n).
-        return false;
+        if (n < 2) { return false; }
+        if (n == 2) { return true; }
+
+        int i = 2;
+        while (i <= (int) Math.ceil(Math.sqrt(n))) {
+            if(n % i == 0) { return false; }
+            i++;
+        }
+
+        return true;
     }
 
     /**
@@ -25,7 +35,21 @@ public class Primes {
      * @return The k-th prime number
      */
     public static int kthPrime(int k) {
-        return 0;
+        // Be sure to interpret num[0] = 2, num[1] = 3, ... num[k-2] = k
+        // Possible prime numbers are marked true
+        boolean[] num = new boolean[k];
+        Arrays.fill(num, true);
+
+        // Mark all multiples of p as non-prime (i.e. false)
+        for (int p = 2; p*p <= k; p++) {
+            if(isPrime(p)) {
+                for (int n = p*2; n <= k; n += p) {
+                    num[n] = false;
+                }
+            }
+        }
+
+        //
     }
 
     /**
